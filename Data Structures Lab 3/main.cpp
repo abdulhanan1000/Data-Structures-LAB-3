897781ba8bf0e402ca4302c6a1636a53b8d13231
Abdul Hanan <abdulhananjavaid10@gmail.com>
2020-03-07 14:54:24 +0500

    Data Structures LAB 3

--- Data Structures Lab 3/main.cpp
+++ Data Structures Lab 3/main.cpp
@@ -66,7 +66,7 @@ using namespace std;
 
 //*****************CHECK IF THERE'S A DIAGNAL IN 2D ARRAY*******************
 
-int main() {
+ /*int main() {
 	int arr[3][3];
 	cout << "enter 2d array values: " << endl;
 	int j = 0;
@@ -117,4 +117,75 @@ int main() {
 
 		cout << "No diagnal\n";
 	}
+}*/
+
+
+
+//********************2D ARRAY MULTIPLICATION******************
+int main() {
+
+
+
+	int arr1[3][3];
+	int arr2[3][3];
+	int arr3[3][3];
+
+	cout << "Enter first 2d array: " << endl;
+	
+
+
+	for (int i = 0; i < 3; i++) {
+
+
+		for (int j = 0; j < 3; j++) {
+
+			cin >> arr1[i][j];
+		}
+
+
+	}
+
+
+	cout << "\nEnter 2nd 2D array: " << endl;
+	for (int i = 0; i < 3; i++) {
+
+
+		for (int j = 0; j < 3; j++) {
+
+			cin >> arr2[i][j];
+		}
+
+
+	}
+
+
+	cout << "\n Performing multiplication: " << endl;
+	
+	for (int i = 0; i < 3; i++) {
+		int k = 0;
+
+		for (int j = 0; j < 3; j++) {
+			
+			
+			arr3[i][j]=(arr1[i][j] * arr2[j][i])+(arr1[k][j] * arr2[j][i]);
+			k++;
+			
+		}
+		
+
+
+	}
+
+	for (int i = 0; i < 3; i++) {
+
+
+		for (int j = 0; j < 3; j++) {
+
+			cout << arr3[i][j] << "  ";
+		}
+		cout << endl;
+
+
+	}
+
 }
